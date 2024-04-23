5925808c429a9840c66321fb583f66f0d6fd75b6
moaiz <moaiz0404@gmail.com>
2024-04-23 19:25:54 +0500

    debugging

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,7 @@
-
 from django.urls import path
 from accounts.views import RegistrationView,ActivateView
 
 urlpatterns = [
-    
     path("register/",RegistrationView.as_view(), name="register"),
-    path('account/activate/<str:uid>/<str:token>/', ActivateView.as_view(), name='activate'),
+    path('accounts/activate/<str:uid>/<str:token>/', ActivateView.as_view(), name='activate'),
 ]
--- main/settings.py
+++ main/settings.py
@@ -26,9 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "accounts",
     "corsheaders",
     'rest_framework',
+    "accounts",
 ]
 
 MIDDLEWARE = [
